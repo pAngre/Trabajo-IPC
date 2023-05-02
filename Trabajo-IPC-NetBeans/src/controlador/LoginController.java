392b33bac9dd41b0c4fa614844d4b6c146e0c15f
pAngre <pabloangre@gmail.com>
2023-05-02 19:42:45 +0200

    -

--- Trabajo-IPC-NetBeans/src/controlador/LoginController.java
+++ Trabajo-IPC-NetBeans/src/controlador/LoginController.java
@@ -5,15 +5,21 @@
 package controlador;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -47,7 +53,19 @@ public class LoginController implements Initializable {
     }    
 
     @FXML
-    private void irARegistro(ActionEvent event) {
+    private void irARegistro(ActionEvent event) throws IOException {
+        FXMLLoader miCargador = new FXMLLoader(getClass().getResource("/vista/Registro.fxml"));
+        Parent root = miCargador.load();
+        
+        RegistroController controladorRegistro = miCargador.getController();
+        
+        Scene scene = new Scene(root, 600, 500);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.setTitle("Registro de Usuario Nuevo");
+        stage.setResizable(false);
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.showAndWait();
     }
 
     @FXML
