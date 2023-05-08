984212606df81bc2fce79dd33735e03561eb96d9
pAngre <pabloangre@gmail.com>
2023-05-08 20:24:02 +0200

    -

--- Trabajo-IPC-NetBeans/src/controlador/PrincipalLoginController.java
+++ Trabajo-IPC-NetBeans/src/controlador/PrincipalLoginController.java
@@ -5,11 +5,15 @@
 package controlador;
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.stage.Stage;
 
 /**
  * Controlador de la ventana PrRINCIPAL con el LOGIN hecho
@@ -31,6 +35,15 @@ public class PrincipalLoginController implements Initializable {
 
     @FXML
     private void clickedSalir(ActionEvent event) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirmacion");
+        alert.setHeaderText(null);
+        alert.setContentText("Seguro que quieres salir?");
+        Optional<ButtonType> result = alert.showAndWait();
+        if(result.get() == ButtonType.OK){
+            Stage stage = (Stage) botonSalir.getScene().getWindow();
+            stage.close();
+        }
     }
     
 }
